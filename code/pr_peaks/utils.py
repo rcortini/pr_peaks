a24ba0d31cc6db987a84f3d1d3cf1f5272fe8210
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-02-26 09:41:23 +0100

    fixed import in utils.py

--- code/pr_peaks/utils.py
+++ code/pr_peaks/utils.py
@@ -1,5 +1,6 @@
 from mybiotools import parse_simple_bed, chipseq_bam_location
 import pysam
+import numpy as np
 
 def load_hcp_peaks (peaks_id,xavi_datadir='/mnt/xavi') :
     datadir = '%s/projects/gvicent/2017-01-23_characterisation_prbs_r5020_titration/tables'%(xavi_datadir)
