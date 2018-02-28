44fdcb601fd32607fd457b2a5be9eedde47ff799
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-02-28 14:36:14 +0100

    chair model now without T matrix but with contacts

--- code/pr_peaks/chair_model.py
+++ code/pr_peaks/chair_model.py
@@ -1,50 +1,36 @@
 import numpy as np
 
-class Searcher :
-    def __init__(self,index,site,td) :
-        self.index = index
-        self.site = site
-        self.td = td
-
-def init_searchers(omega_t,site_taus,deterministic=False) :
-    """
-    Initializes the searchers in the system. Note that this function will only
-    be invoked from within the main simulation loop, so the "searchers" list that
-    will be created is only visible internally, as a convenient data structure to
-    keep track of what's going on, and not to the external world to see.
-    """
-    # get parameters from the omega_t matrix
-    n = omega_t.size
-    m = omega_t.sum()
-    # init the searchers
-    searchers = []
-    # "searcher_sites" is a vector of indices containing the indices
-    # of the sites occupied by the searchers
-    searcher_sites = np.where(omega_t)[0]
-    for s in xrange(m) :
-        site = searcher_sites[s]
-        if deterministic :
-            td = site_taus[site]
-        else :
-            td = np.random.exponential(scale=site_taus[site])
-        searcher = Searcher(s,site,td)
-        searchers.append(searcher)
-    return searchers
-
-def run_chair_simulation(nsteps,omega_t_initial,T,site_taus,
-                         seed=None,teq=0,tsample=1,deterministic=False) :
+def init_omega_t(n,mu,seed=None) :
     # init the random number generator if it was passed
     if seed is not None :
         np.random.seed(seed)
-    # make an internal copy of the initial omega_t matrix
+    # init omega vector
+    omega_t = np.zeros(n,dtype=bool)
+    # fill with initial occupancy
+    omega_t[np.random.choice(n,mu,replace=False)] = True
+    return omega_t
+
+def run_chair_simulation(nsteps,omega_t_initial,H,site_taus,boost,
+                         seed=None,teq=0,tsample=1) :
+    # init the random number generator if it was passed
+    if seed is not None :
+        np.random.seed(seed)
+    # make an internal copy of the initial omega_t vector
     omega_t = omega_t_initial.copy()
     # no need to pass N,n,m through the arguments of the function
     n = omega_t.size
     m = omega_t.sum()
     # init jumping matrix
     J = np.zeros((n,n)).astype(np.int32)
-    # init searchers
-    searchers = init_searchers(omega_t,site_taus,deterministic=deterministic)
+    # init searcher_sites
+    searcher_sites = -1 * np.ones(n,dtype=np.int32)
+    searcher_sites[omega_t] = np.arange(m)
+    # init searcher_times
+    searcher_times = np.zeros(m)
+    for s in xrange(m) :
+        searcher_id = searcher_sites[s]
+        if searcher_id != -1 :
+            searcher_times[searcher_id] = site_taus[s]
     # init sampling matrix
     nsamples = (nsteps-teq)/tsample
     samples = np.zeros((nsamples,n),dtype=bool)
@@ -52,65 +38,52 @@ def run_chair_simulation(nsteps,omega_t_initial,T,site_taus,
     # cycle on time
     for step in xrange(1,nsteps+1) :
         # cycle on the searchers
-        for s in xrange(m) :
-            searcher = searchers[s]
-            # if the searcher has to stay longer on the site, skip it
-            if step>=searcher.td :
-                # if not, get the elements corresponding to the transition matrix
-                Tstar = T[searcher.site,:] * (~omega_t)
-                Tstar /= Tstar.sum()
-                # now get the next site
-                next_site = np.random.choice(n,p=Tstar)
-                # update omega matrix
-                omega_t[searcher.site] = False
-                omega_t[next_site] = True
-                # update jumping matrix
-                J[searcher.site,next_site] += 1
-                # update searcher
-                searcher.site = next_site
-                if deterministic :
-                    next_td = site_taus[searcher.site]
-                else :
-                    next_td = np.random.exponential(scale=site_taus[searcher.site])
-                searcher.td = step + next_td
+        for s in xrange(n) :
+            searcher_id = searcher_sites[s]
+            if searcher_id != -1 :
+                # if the searcher has to stay longer on the site, skip it
+                if step>=searcher_times[searcher_id] :
+                    # now get the next site
+                    next_site = np.random.choice(np.where(~omega_t)[0])
+                    # update omega matrix
+                    omega_t[s] = False
+                    omega_t[next_site] = True
+                    # update jumping matrix
+                    J[s,next_site] += 1
+                    # update searcher_sites
+                    searcher_sites[s] = -1
+                    searcher_sites[next_site] = searcher_id
+                    # update searcher_times
+                    next_td = site_taus[next_site]
+                    for contact in H[next_site] :
+                        contact_searcher_id = searcher_sites[contact]
+                        if contact_searcher_id != -1 :
+                            next_td *= boost
+                            searcher_times[contact_searcher_id] = step + next_td
+                            break
+                    searcher_times[searcher_id] = step + next_td
         # update samples
         if step>teq and (step-teq)%tsample==0 :
             samples[i_sample,:] = omega_t
             i_sample += 1
     return omega_t, J, samples
 
-def init_omega_t(n,mu,sigma=None,seed=None) :
-    # init the random number generator if it was passed
-    if seed is not None :
-        np.random.seed(init_seed)
-    # init searcher numbers
-    if sigma is not None :
-        m = np.random.normal(loc=mu,scale=sigma).astype(np.int32)
-        # ensures that the system has more searchers than available sites and that
-        # it has at least one searcher
-        if m>n : m=n
-        if m<1 : m=1
-    else :
-        m = mu
-    # init omega vector
-    omega_t = np.zeros(n,dtype=bool)
-    # fill with initial occupancy
-    omega_t[np.random.choice(n,m,replace=False)] = True
-    return omega_t
-
 class JumpingModel :
-    def __init__ (self,T,site_taus) :
-        self.T = T
+    def __init__ (self,H,site_taus,boost) :
+        self.H = H
         self.site_taus = site_taus
-        self.omega_t = {}
+        self.boost = boost
         self.J = {}
         self.samples = {}
         self.theta = {}
-    def run(self,nsteps,mu,sigma,omega_t_initial,
-           seed=None,teq=0,tsample=1,deterministic=False) :
-        self.omega_t[mu],self.J[mu],self.samples[mu] = \
-                run_chair_simulation(nsteps,omega_t_initial,self.T,self.site_taus,
-                                    seed=seed,teq=teq,tsample=tsample,deterministic=deterministic)
+    def run(self,nsteps,omega_t_initial,seed=None,teq=0,tsample=1) :
+        omega_t,self.J[mu],self.samples[mu] = \
+            run_chair_simulation(nsteps,
+                                 omega_t_initial,
+                                 self.H,
+                                 self.site_taus,
+                                 self.boost,
+                                 seed=None,teq=0,tsample=1)
         self.theta[mu] = self.samples[mu].sum(axis=0)/float(self.samples[mu].sum())
 
 def H_to_L(model,Hsites,Lsites) :
